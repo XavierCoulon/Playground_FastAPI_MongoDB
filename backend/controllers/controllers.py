2949e6820ad76c622b86ff2dab19f03b0a1db986
XavierCoulon <xcoulon.mail@gmail.com>
2023-01-13 16:10:36 +0100

    fix scraping

--- backend/controllers/controllers.py
+++ backend/controllers/controllers.py
@@ -1,8 +1,8 @@
 import re
 from pprint import pprint
-from config.db import db
 from controllers.soup import Soup
 from config.constants import URI
+from config.db import db
 from tqdm import tqdm
 
 def get_links():
@@ -18,16 +18,44 @@ def extract_data_from_page(url):
 	
 	try:	
 		model = soup.find_all("h1", itemprop="name")[0].string.strip('\t\r\n').strip()
+	except (IndexError, AttributeError) as e:
+		print(e.__str__())
+		print(f"Model is missing on ${url}")
+	try:	
 		brand = soup.find_all("a", itemprop="brand")[0].string.strip('\t\r\n').strip()
-		motorisation = soup.find_all("h5", itemprop="fuelType")[0].string.strip('\t\r\n').strip()
-		autonomy = soup.find_all("h5", text= re.compile("Autonomie"))[0].find_next("span").text.strip('\t\r\n').strip()
+	except (IndexError, AttributeError) as e:
+		print(e.__str__())
+		print(f"Brand is missing on ${url}")
+	try:	
 		image_model = soup.find_all("img", itemprop="image")[0]["src"]
+	except (IndexError, AttributeError) as e:
+		print(e.__str__())
+		print(f"Model's image is missing on ${url}")
+	try:	
+		motorisation = soup.find_all("h5", itemprop="fuelType")[0].string.strip('\t\r\n').strip()
+	except (IndexError, AttributeError) as e:
+		print(e.__str__())
+		print(f"Motorisation is missing on ${url}")
+	try:	
+		autonomy = soup.find_all("h5", text= re.compile("Autonomie"))[0].find_next("span").text.strip('\t\r\n').strip()
+	except (IndexError, AttributeError) as e:
+		print(e.__str__())
+		print(f"Autonomy is missing on ${url}")
+	try:	
 		image_brand = soup.find_all("img", class_="g-width-70x")[0]["src"]
+	except (IndexError, AttributeError) as e:
+		print(e.__str__())
+		print(f"Brand's image is missing on ${url}")
+	try:	
 		price = int("".join(filter(str.isdigit, soup.find_all("h5", text="Prix")[0].find_next("span").text)))
+	except (IndexError, AttributeError) as e:
+		print(e.__str__())
+		print(f"Price is missing on ${url}")
+	try:	
 		rating = soup.find_all("div", class_="glsr-star-rating glsr-stars")[0].find_next("span").find_next("span").text.strip('\t\r\n').strip()
 	except (IndexError, AttributeError) as e:
 		print(e.__str__())
-		print(url)
+		print(f"Rating is missing on ${url}")
 
 	return {"model": model, "brand": brand, "motorisation": motorisation, "autonomy": autonomy, "image_model": image_model, "price": price, "image_brand": image_brand, "rating": rating}
 	
@@ -43,6 +71,6 @@ def repopulate_db():
 	print("...then database populated!")
 
 if __name__ == "__main__":
-	pprint(extract_data_from_page("https://www.automobile-propre.com/voitures/bmw-740e-iperformance/"))
-
+	pprint(extract_data_from_page("https://www.automobile-propre.com/voitures/peugeot-508-hybrid4/"))
+	
 
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from routes.car import car
+from controllers.controllers import repopulate_db, extract_data_from_page
 
 app = FastAPI()
 app.include_router(car, prefix="/api/v1")
 
-
-
-
-
-
+repopulate_db()
+# print(extract_data_from_page("https://www.automobile-propre.com/voitures/peugeot-508-hybrid4/"))
